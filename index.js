9368c89fe35bfde98bf912947f8a8ce538884a74
JesseNReynolds <jessenreynolds@gmail.com>
2021-01-19 13:05:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+
+function map(arr, fn) {
+    let working = []
+    arr.forEach(element => {
+        working.push(fn(element))
+    });
+    return working
+}
+
+function reduce(arr, fn, start) {
+    
+    if (start) {
+       arr.forEach(element => {
+            start = fn(element, start)
+        })
+    } else {
+        start = arr[0]
+        for (let i = 1; i < arr.length; i++) {
+            start = fn(arr[i], start)
+        }
+    }
+    return start
+}
